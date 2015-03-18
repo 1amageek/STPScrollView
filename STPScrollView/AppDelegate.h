7e751b5ef6cd857c493f43ca80bea46cc99c3c23
NORI <tmy0x3@icloud.com>
2015-03-18 09:50:30 +0900

    Initial Commit

---
+++ STPScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  STPScrollView
+//
+//  Created by Norikazu on 2015/03/18.
+//  Copyright (c) 2015年 Stamp inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ STPScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  STPScrollView
+//
+//  Created by Norikazu on 2015/03/18.
+//  Copyright (c) 2015年 Stamp inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
